c900adfde2bff70fbbc42e835040e5634a4370a5
Joshua Melink <melinkjl@muohio.edu>
2012-08-26 23:57:07 -0400

    Background changes from red to yellow to white

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -13,12 +13,16 @@ class CatPictureApp : public AppBasic {
 	void draw();
 
   private:
-	float brightness;
+	float red;
+	float green;
+	float blue;
 };
 
 void CatPictureApp::setup()
 {
-	brightness = 0.0f;
+	red = 0.0f;
+	green = 0.0f;
+	blue = 0.0f;
 }
 
 void CatPictureApp::mouseDown( MouseEvent event )
@@ -27,17 +31,29 @@ void CatPictureApp::mouseDown( MouseEvent event )
 
 void CatPictureApp::update()
 {
-	brightness = brightness + 0.01f;
-	if(brightness > 1.0f)
+	red = red + 0.005f;
+	if(red > 1.0f)
 	{
-		brightness = 0.0f;
+		red = red - 0.005f;
+		green = green + 0.005f;
+		if (green > 1.0f)
+		{
+			green = green - 0.005f;
+			blue = blue + 0.005f;
+			if (blue > 1.0f)
+			{
+				red = 0.0f;
+				green = 0.0f;
+				blue = 0.0f;
+			}
+		}
 	}
 }
 
 void CatPictureApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( brightness, brightness, brightness ) ); 
+	gl::clear( Color( red, green, blue ) ); 
 }
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
